3bf9037c4c278e5b189af1fa1902cb49cbfaeedf
atharvad97 <91610532+atharvad97@users.noreply.github.com>
2021-12-24 11:06:39 +0530

    Add: Selection sort in C

---
+++ Data Structures/Array/Sorting/SelectionSort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+   int arr[10]={5,48,69,35,21,61,27,6,32,45};
+   int n=10;
+   int i, j, position, swap;
+   for (i = 0; i < (n - 1); i++) {
+      position = i;
+      for (j = i + 1; j < n; j++) {
+         if (arr[position] > arr[j])
+            position = j;
+      }
+      if (position != i) {
+         swap = arr[i];
+         arr[i] = arr[position];
+         arr[position] = swap;
+      }
+   }
+   for (i = 0; i < n; i++)
+      printf("%d\t", arr[i]);
+   return 0;
+}
